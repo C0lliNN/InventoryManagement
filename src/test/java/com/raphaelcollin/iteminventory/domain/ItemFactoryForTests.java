f4bf1a74a7d52c1b481030057b3449e726b66a9c
C0lliNN <rapphaelmanhaes2017@hotmail.com>
2021-06-06 13:09:11 -0300

    Create ItemSerializerTest

--- src/test/java/com/raphaelcollin/iteminventory/domain/ItemFactoryForTests.java
+++ src/test/java/com/raphaelcollin/iteminventory/domain/ItemFactoryForTests.java
@@ -1,5 +1,7 @@
 package com.raphaelcollin.iteminventory.domain;
 
+import com.raphaelcollin.iteminventory.infrastructure.mongodb.document.ItemDocument;
+
 import java.math.BigDecimal;
 import java.util.UUID;
 
@@ -14,5 +16,15 @@ public class ItemFactoryForTests {
                 .price(BigDecimal.valueOf(23))
                 .quantity(2)
                 .build();
+    }
+
+    public static ItemDocument newItemDocument() {
+        return new ItemDocument(
+                UUID.randomUUID().toString(),
+                UUID.randomUUID().toString(),
+                UUID.randomUUID().toString(),
+                BigDecimal.valueOf(23),
+                2
+        );
     }
 }
