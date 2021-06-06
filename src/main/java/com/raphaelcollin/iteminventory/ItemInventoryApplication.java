ba0c35ae55449a37b4c815e7144c5bf824737836
C0lliNN <rapphaelmanhaes2017@hotmail.com>
2021-06-06 12:16:26 -0300

    Initialize Project

---
+++ src/main/java/com/raphaelcollin/iteminventory/ItemInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.raphaelcollin.iteminventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemInventoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItemInventoryApplication.class, args);
+    }
+
+}
